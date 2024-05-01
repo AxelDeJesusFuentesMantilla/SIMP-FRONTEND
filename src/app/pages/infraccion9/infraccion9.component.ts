ac7696d93e54477c42c1a27f7cf769776905768f
ricascor080 <116040550+ricascor080@users.noreply.github.com>
2024-04-30 22:42:07 -0600

    Cambios
    cambios

--- src/app/pages/infraccion9/infraccion9.component.ts
+++ src/app/pages/infraccion9/infraccion9.component.ts
@@ -9,12 +9,32 @@ import jsPDF from 'jspdf';
 export class Infraccion9Component {
 
   downloadPdf() {
-    const doc = new jsPDF();
+    const doc = new jsPDF({
+      orientation: 'portrait',
+      unit: 'mm',
+      format: [210, 210]  // Formato de página cuadrada para el ejemplo
+    });
 
-    // Suponiendo que el texto que quieres imprimir está en un elemento con el ID 'content'
-    const content = document.getElementById('content')?.innerText;
+    // Cargar e insertar el primer logo
+    const imgData1 = 'assets/images/logo1.png';  // Asegúrate de tener el path correcto
+    doc.addImage(imgData1, 'PNG', 10, 5, 20, 10);  // Cambia las dimensiones según sea necesario
 
-    doc.text(content || '', 10, 10);
+    // Cargar e insertar el segundo logo
+
+
+    // Texto de ejemplo y contenido del documento
+    doc.setFont('helvetica', 'bold');
+    doc.text('MOVILIA', 100, 10);  // Posición central superior del texto
+
+    const contentElement = document.getElementById('content');
+    const contentText = contentElement ? contentElement.innerText : '';
+
+    doc.setFontSize(3);
+    doc.text(contentText, 1, 20, {
+      maxWidth: 7
+    });
+
+    // Guardar el PDF
     doc.save('Infraccion.pdf');
   }
 }
